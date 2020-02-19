51904c544137665392ea4ac4142e3b0e1889bcc8
taysands <59378939+taysands@users.noreply.github.com>
2020-02-19 18:08:54 -0500

    Update javascriptgame.js

--- javascriptgame.js
+++ javascriptgame.js
@@ -1,30 +1,48 @@
-let gamePlay = prompt ("Want To Play The Game? [YES] or [NO]?");
-let userName = "";
-  
-if(gamePlay === "Yes" || gamePlay === "YES" || gamePlay === "[YES]" || gamePlay === "[Yes]" || gamePlay === "[yes]" || gamePlay === "yes"){
-   let userName = prompt ("Great, Please Enter Your Name");
-  }
-  else if(gamePlay === "No" || gamePlay === "NO" || gamePlay === "[NO]" || gamePlay === "[No]" || gamePlay === "[no]" || gamePlay === "no") {
-    console.log("Come back and enter [YES] when you are ready to battle!")
-  }
-  else {
-    console.log("NOT VAILD! - PLEASE ENTER: [YES] or [NO]")
-  }
+let userPlayer = "Player One";
+let userPoints = 40;
+let grantPoints = 10;
+let grantLoses = 3
+let randomPoints = Math.floor(Math.random()* 4)+1;
+let alertCombat;
 
-let gameStart = function(userName){
-  let userPoints = 40;
-  let grantPoints = 10;
-  let grantLoses = 0;
-  let minusPoints = Math.floor(Math.random()* -2)-0;
+
+let startCombat = function(){
+  let gamePlay = prompt("Would you like to play the game?");
+if(gamePlay === "Yes" || gamePlay === "YES" || gamePlay === "[YES]" || gamePlay === "[Yes]" || gamePlay === "[yes]" || gamePlay === "yes"){
+  userPlayer = prompt("Ok, Please enter your name");
+}
+else if(gamePlay === "No" || gamePlay === "NO" || gamePlay === "[NO]" || gamePlay === "[No]" || gamePlay === "[no]" || gamePlay === "no"){
+  console.log("Come back and enter [YES] when you are ready to battle!")
+}
+else{
+  gamePlay = alert("*INVALID RESPONSE* To Battle Enter [YES}");
+}
+
+if(userPlayer = userPlayer){
+  let alertCombat = alert("Ok, " + userPlayer + " Let's Battle");
+}
+else{
+  gamePlay = alert("*INVALID RESPONSE* you must [ENTER A NAME} To Battle");
+ }
+}
+
+let getDamage = function(startCombat, grantPoints, grantLoses){
   
-  while(userPoints > 0 || grantLoses === 3){
-   let userPoints = userPoints + minusPoints;
-    let grantPoints = grantPoints + minusPoints;
+  grantPoints = 10;
+  
+  while(userPoints > 0 || grantLoses === 0){
+    let battlePrompt = alert('Click "OK" to Attack')
+    grantPoints = grantPoints - randomPoints;
+    userPoints = userPoints - randomPoints;
     if(grantPoints === 0 && userPoints !== 0){
-      grantsLoses++;
-      let grantsPoints = 10;
+      grantsLoses--; break;
+      grantPoints = 10;
     }
-    console.log(userName + "has" + userPoints + " health left")
-    console.log("Grant the Mighty Chicken has" + grantPoints + " health left")
+    
+    console.log(userPlayer + " has " + userPoints + " Points Left In This Battle")
+    console.log("Grant the Almighty Chicken has " + grantPoints + " Points Left In This Battle")
   }
 }
+
+console.log(startCombat())
+console.log(getDamage())
